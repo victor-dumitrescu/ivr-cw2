582ea71eaf892db338a0a4393ca8e82bd8adf32d
victor-dumitrescu <vichvymetal@gmail.com>
2014-03-20 19:52:06 +0000

    Test controller, updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ivr-cw2
 =======
+
+###Setup
+
+To run the system, start `webots` from terminal, then open the world file from `kheperam/worlds/khepera.wbt`. This will also open the controller defined in the file.
+
+You can add a controller in `kheperam/controllers` and then change the controller variable in `khepera.wbt`.
+
+------
--- kheperam/controllers/test_controller/test_controller.m
+++ kheperam/controllers/test_controller/test_controller.m
@@ -22,7 +22,7 @@ end
 
 % Calling MATLAB desktop version
 % wb_differential_wheels_set_speed(1, -1);
-wb_differential_wheels_set_speed(1, -1);
+wb_differential_wheels_set_speed(10, -10);
 desktop;
 
 % main loop:
@@ -35,16 +35,17 @@ while wb_robot_step(TIME_STEP) ~= -1
            sensor_values(i) = wb_distance_sensor_get_value(ps(i));
        end
   % display all distance sensors values
-       sensor_values
+       % sensor_values
+
   left_speed=1;
   right_speed=1;
   %set_speeds(left_speed,right_speed);
   if sensor_values(1)+sensor_values(2)+sensor_values(3)>10
     wb_differential_wheels_set_speed(1, -1);
   elseif sensor_values(4)+sensor_values(5)+sensor_values(6)>10
-    wb_differential_wheels_set_speed(-1, 1);
+    wb_differential_wheels_set_speed(-10, 10);
   else
-     wb_differential_wheels_set_speed(1, 1);
+     wb_differential_wheels_set_speed(10, 10);
   end;
   %control goes to the keyboard
   %keyboard;
