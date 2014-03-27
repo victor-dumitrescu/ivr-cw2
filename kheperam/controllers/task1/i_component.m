d7501109381e39b8535de9313089612f9e3ff946
Ignotas Sulzenko <tasignotas@gmail.com>
2014-03-27 00:46:35 +0000

    A bit refactored, not sure if still works

--- kheperam/controllers/task1/i_component.m
+++ kheperam/controllers/task1/i_component.m
@@ -2,11 +2,11 @@ function [add_right, add_left, new_error_history] = ...
          i_component (error_values, error_history)
     % Integral component of the PID controller.
     I_GAIN = [0 0 0 0 0.0001 0.0001 0 0];
-    epsilon = 10 * ones(1,8);
+    epsilon = 10 * ones(1,SENSOR_COUNT);
 
     if all(abs(error_values) < epsilon)
         % Reset error history
-        new_error_history = zeros(1, 8);
+        new_error_history = zeros(1, SENSOR_COUNT);
         add_right = 0;
         add_left = 0;
     else
@@ -15,12 +15,11 @@ function [add_right, add_left, new_error_history] = ...
         add_left = 0;
         add_right = 0;
 
-        for i=1:8
+        for i=1:SENSOR_COUNT
             if new_error_history(i) > 0
                 add_left = add_left + new_error_history(i);
             else
                 add_right = add_right - new_error_history(i);
             end
     end
-
 end
